4b9dcb80e6577f0862b4e996aa31d1d7f12e58f3
pingu543 <pcu50208@outlook.com>
2025-02-25 11:19:01 -0500

    Update SnakeMovement.cs
    Snake's direction will change when a perpendicular direction key is pressed (disregarding regular movement intervals). Input in the same or opposite direction will not trigger the direction change.

--- Assets/SnakeMovement.cs
+++ Assets/SnakeMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class SnakeMovement : MonoBehaviour {
+public class SnakeMovement : MonoBehaviour
+{
     public float moveSpeed = 5f; // Speed of movement
     public Vector2Int gridSize = new Vector2Int(32, 32); // Grid size
     private Vector2Int direction = Vector2Int.zero; // Default direction
@@ -8,30 +9,51 @@ public class SnakeMovement : MonoBehaviour {
     private float moveTimer = 0f;
     public float moveInterval = 0.5f; // Time between movement steps
 
-    void Update() {
+    private bool inputReceived = false;
+
+    void Update()
+    {
         HandleInput();
     }
 
-    void FixedUpdate() {
+    // fixedUpdate is called at a fixed interval, independent of frame rate
+    void FixedUpdate()
+    {
         MoveSnake();
     }
 
-    void HandleInput() {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && direction != Vector2Int.down)
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && direction != Vector2Int.down && direction != Vector2Int.up)
+        {
             direction = Vector2Int.up;
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector2Int.up)
+            inputReceived = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector2Int.up && direction != Vector2Int.down)
+        {
             direction = Vector2Int.down;
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector2Int.right)
+            inputReceived = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector2Int.right && direction != Vector2Int.left)
+        {
             direction = Vector2Int.left;
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != Vector2Int.left)
+            inputReceived = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != Vector2Int.left && direction != Vector2Int.right)
+        {
             direction = Vector2Int.right;
+            inputReceived = true;
+        }
     }
 
-    void MoveSnake() {
+    void MoveSnake()
+    {
         moveTimer += Time.fixedDeltaTime;
-        if (moveTimer >= moveInterval) {
+        if (moveTimer >= moveInterval || inputReceived == true)
+        {
             transform.position += new Vector3(direction.x * gridSize.x, direction.y * gridSize.y, 0);
             moveTimer = 0f;
+            inputReceived = false;
         }
     }
 }
